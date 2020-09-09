9ba85035879b531e791724505aaea8e3817aa504
John Clary <john.clary@austintexas.gov>
2020-09-08 19:44:04 -0500

    s3 tests ready

--- tests/test_utils_s3.py
+++ tests/test_utils_s3.py
@@ -1,10 +1,15 @@
 import io
 import json
+import os
+
+import arrow
+import boto3
 import pytest
 from services.utils import s3
 
 BUCKET_NAME = "atd-knack-services"
-FILENAME = "dev/test_record.json"
+PREFIX = "dev/_tests"
+FILENAME = os.path.join(PREFIX, "test_record.json")
 APP_NAME = "tests"
 
 
@@ -33,3 +38,33 @@ def test_upload(record_package):
     results = s3.upload([record_package])
     # results should = [None]
     assert not any(results)
+
+
+def test_download_one(record_data):
+    data = s3.download_one(bucket_name=BUCKET_NAME, fname=FILENAME)
+    assert data == record_data
+
+
+def test_download_many(record_data):
+    data = s3.download_many(bucket_name=BUCKET_NAME, prefix=PREFIX)
+    assert data[0] == record_data
+
+
+def test_download_many_date_filter_1(record_data):
+    """Query S3 for records modified 1 hour from now"""
+    resource = boto3.resource("s3")
+    bucket = resource.Bucket(BUCKET_NAME)
+    objs_all = bucket.objects.filter(Prefix=PREFIX)
+    now = arrow.now().shift(hours=1).format()
+    filtered_objs = s3.filter_by_date(objs_all, now)
+    assert len(filtered_objs) == 0
+
+
+def test_download_many_date_filter_1(record_data):
+    """Query S3 for records modified 1 hour ago"""
+    resource = boto3.resource("s3")
+    bucket = resource.Bucket(BUCKET_NAME)
+    objs_all = bucket.objects.filter(Prefix=PREFIX)
+    now = arrow.now().shift(hours=-1).format()
+    filtered_objs = s3.filter_by_date(objs_all, now)
+    assert len(filtered_objs) == 1
