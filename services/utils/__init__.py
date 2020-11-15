8814a5636518c31c9a5678db22528a6ee702434e
John Clary <john.clary@austintexas.gov>
2020-11-14 22:47:28 -0600

    create agol utils

--- services/utils/__init__.py
+++ services/utils/__init__.py
@@ -1,9 +1,10 @@
-from . import args, knack, s3, socrata, logging
+from . import agol, args, logging, knack, s3, socrata
 
 __all__ = [
+    "agol",
     "args",
+    "logging",
     "knack",
     "s3",
     "socrata",
-    "logging"
 ]
