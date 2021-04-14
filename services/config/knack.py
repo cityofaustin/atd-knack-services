f43fc30c5631d5ea67c7f6fa82467b2ec98e519c
Chia Berry <chia.berry@austintexas.gov>
2021-04-14 15:19:14 -0500

    correct view for flashing beacons

--- services/config/knack.py
+++ services/config/knack.py
@@ -79,12 +79,12 @@ CONFIG = {
             "socrata_resource_id": "hst3-hxcz",
             "location_field_id": "field_182",
         },
-        "view_1454": {
+        "view_1563": {
             "description": "Flashing Beacons",
-            "scene": "scene_519",
+            "scene": "scene_568",
             "modified_date_field": "field_1701",
             "socrata_resource_id": "wczq-5cer",
-            "location_field_id": "field_211",
+            "location_field_id": "field_182",
             "service_id": "6c4392540b684d598c72e52206d774be",
             "layer_id": 0,
             "item_type": "layer",
