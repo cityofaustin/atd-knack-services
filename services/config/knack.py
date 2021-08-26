91afafc284f155fe9d94aa16b239a2285d711291
John Clary <john.clary@austintexas.gov>
2021-08-26 15:25:05 -0500

    e

--- services/config/knack.py
+++ services/config/knack.py
@@ -222,7 +222,7 @@ CONFIG = {
         },
        # Note that views 3307 and 3528 push to the same socrata dataset. This object is a child to
         # both work_orders_markings and work_orders_signs - which share duplicate field names, notably
-        # ATD_WORK_ORDER_ID, WORK_TYPE, and LOCATION_NAME. So we source the tim logs from two
+        # ATD_WORK_ORDER_ID, WORK_TYPE, and LOCATION_NAME. So we source the time logs from two
         # similar views, one with connection fields added from markings and the other with fields
         # added from signs. They map one set of columns in Socrata, which matches on the knack field
         # name rather than key.
