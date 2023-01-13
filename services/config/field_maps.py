10b2f8c69b4ac30afe031f06f361e1158cc77288
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-01-13 12:33:25 -0600

    signals to artbox publish config

--- services/config/field_maps.py
+++ services/config/field_maps.py
@@ -29,35 +29,87 @@ FIELD_MAPS = {
     "data-tracker": {
         "view_197": [
             # Signal ID
-            {"src": "field_199_raw", "vza": "field_751", "primary_key": True},
+            {
+                "src": "field_199_raw",
+                "smart-mobility": "field_380",
+                "primary_key": True,
+            },
             # Location Name
-            {"src": "field_211_raw", "vza": "field_624", "handler": handle_strip},
-            # Signal Type
-            {"src": "field_201", "vza": "field_753",},
+            {
+                "src": "field_211_raw",
+                "smart-mobility": "field_382",
+                "handler": handle_strip,
+            },
             # Signal Status
-            {"src": "field_491", "vza": "field_759",},
-            # Location Type
-            {"src": None, "vza": "field_750", "default": "Signal Location",},
-            # Type
-            {"src": None, "vza": "field_626", "default": "Signal"},
-            # Modifie Date/time
-            {"src": "field_205", "vza": "field_647",},
+            {
+                "src": "field_491",
+                "smart-mobility": "field_383",
+            },
+            # Modified Date/time
+            {
+                "src": "field_205",
+                "smart-mobility": "field_391",
+            },
+            # Location ID
+            {
+                "src": "field_209",
+                "smart-mobility": "field_381",
+            },
+            # WEB_URL_GENERATOR_PM
+            {
+                "src": "field_3399",
+                "smart-mobility": "field_384",
+            },
+            # Council district
+            {
+                "src": "field_1256",
+                "smart-mobility": "field_385",
+            },
+            # Signal Name
+            {
+                "src": "field_1058",
+                "smart-mobility": "field_386",
+            },
+            # Signal Engineer Area
+            {
+                "src": "field_1857",
+                "smart-mobility": "field_387",
+            },
         ],
     },
     "finance-purchasing": {
         "view_788": [
             # ATD_INVENTORY_ID
-            {"src": "field_918", "data-tracker": "field_3812", "primary_key": True,},
+            {
+                "src": "field_918",
+                "data-tracker": "field_3812",
+                "primary_key": True,
+            },
             #  STOCK_NUMBER
-            {"src": "field_720", "data-tracker": "field_3467",},
+            {
+                "src": "field_720",
+                "data-tracker": "field_3467",
+            },
             #  CATEGORY
-            {"src": "field_363", "data-tracker": "field_243",},
+            {
+                "src": "field_363",
+                "data-tracker": "field_243",
+            },
             #  Financial Name
-            {"src": "field_364", "data-tracker": "field_244",},
+            {
+                "src": "field_364",
+                "data-tracker": "field_244",
+            },
             # Common Name
-            {"src": "field_914", "data-tracker": "field_3617",},
+            {
+                "src": "field_914",
+                "data-tracker": "field_3617",
+            },
             # Modified Date
-            {"src": "field_374", "data-tracker": "field_1229",},
+            {
+                "src": "field_374",
+                "data-tracker": "field_1229",
+            },
             # Modified by
             {
                 "src": "field_505_raw",
@@ -65,20 +117,40 @@ FIELD_MAPS = {
                 "handler": handle_connection,
             },
             #  INVENTORY_TRACKING
-            {"src": "field_371", "data-tracker": "field_1125",},
+            {
+                "src": "field_371",
+                "data-tracker": "field_1125",
+            },
             #  STATUS
-            {"src": "field_370", "data-tracker": "field_1068",},
+            {
+                "src": "field_370",
+                "data-tracker": "field_1068",
+            },
             # UNIT_OF_MEASURE
-            {"src": "field_377", "data-tracker": "field_2420",},
+            {
+                "src": "field_377",
+                "data-tracker": "field_2420",
+            },
             # Object code
-            {"src": "field_920", "data-tracker": "field_3462",},
+            {
+                "src": "field_920",
+                "data-tracker": "field_3462",
+            },
             # Re-Order Threshold
-            {"src": "field_922", "data-tracker": "field_3902",},
+            {
+                "src": "field_922",
+                "data-tracker": "field_3902",
+            },
             # Re-order Turnaround Time
-            {"src": "field_923", "data-tracker": "field_3903",},
+            {
+                "src": "field_923",
+                "data-tracker": "field_3903",
+            },
             # Comment
-            {"src": "field_924", "data-tracker": "field_3905",},
+            {
+                "src": "field_924",
+                "data-tracker": "field_3905",
+            },
         ]
     },
 }
-
