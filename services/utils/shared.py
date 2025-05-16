c1598e4a838b0af239ec63f58767137cffa54a54
Charlie Henry <thomashenry@utexas.edu>
2025-05-16 13:30:32 -0500

    special character remover

--- services/utils/shared.py
+++ services/utils/shared.py
@@ -1,7 +1,12 @@
+import re
+
+
 def format_keys(record):
     """Format Knack record keys by converting to lower case and replacing space
-    with underscores"""
+    with underscores. Then, replaces all special characters with underscores."""
     return {
-        key.lower().replace(" ", "_").replace("-", "_"): val
+        re.sub(
+            r"[^a-z0-9_]+", "", key.lower().replace(" ", "_").replace("-", "_")
+        ): val
         for key, val in record.items()
     }
